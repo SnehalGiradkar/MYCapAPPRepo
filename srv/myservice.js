50cae285dfc7bbb101069d51e07e4a993154e982
Snehal Giradkar <snehalgiradkr@gmail.com>
2024-11-13 05:16:09 +0000

    my stable app

---
+++ srv/myservice.js
@@ -0,0 +1,36 @@
+module.exports = (srv) => {
+   
+    srv.on('hello', (req) => {
+        let myName = req.data.name;
+        return "Hello Amigo " + myName;
+    });
+ 
+    // const { EmployeeSrv } = cds.entities;
+ 
+    // srv.on('READ', EmployeeSrv, (req) => {
+    //     return {
+    //         "ID": "ZUMBAID",
+    //         "NAME":"XYZ"
+    //     };
+    // });
+
+    const { employees } = cds.entities("anubhav.db.master");
+ 
+    srv.on('READ', 'EmployeeSrv', async (req) => {
+       
+        const tx = cds.tx(req);
+        var data = await tx.run(SELECT.from(employees).where({
+            "Currency_code": "USD"
+        }).limit(3));
+ 
+        data.push({// if need to push any new data
+            "ID":"Zkas",
+            "nameFirst": "Anil",
+            "nameLast": "Kapoor"
+        });
+       
+        return data;
+ 
+    });
+ 
+}
